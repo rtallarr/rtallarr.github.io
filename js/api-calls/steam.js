79098412be7b0a0f9664fa793531299de1b320fc
rtallar <rtallar@gmail.com>
2023-11-09 03:44:09 -0300

    request to steam over HTTPS

--- js/api-calls/steam.js
+++ js/api-calls/steam.js
@@ -1,4 +1,4 @@
-const apiUrl = ' http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=ABEE14437DCEF213B01CC2A90AE407CD&steamid=76561199081630402&format=json'
+const apiUrl = ' https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=ABEE14437DCEF213B01CC2A90AE407CD&steamid=76561199081630402&format=json'
 
 async function getOwnedGames() {
     const response = await fetch(apiUrl);
