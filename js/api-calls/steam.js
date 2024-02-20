f78584aa333af9fce7e471c5422f2d8718a4b5e4
Rodrigo Tallar <rtallarr@gmail.com>
2024-02-20 01:50:47 -0300

    fix: make steam api call to custom server

--- js/api-calls/steam.js
+++ js/api-calls/steam.js
@@ -1,9 +1,8 @@
-const apiUrl = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=3C4B81AC1341397A4D27CB13BAE3D6AE&steamid=76561199081630402&format=json'
+const apiUrl = 'https://rtback.azurewebsites.net/steam/gameCount'
 
 async function getOwnedGames() {
     const response = await fetch(apiUrl);
-    let data = await response.json()
-    return data.response.game_count;
+    return response.text();
 }
 
 getOwnedGames().then(data => 
--- js/utils/age.js
+++ js/utils/age.js
@@ -2,6 +2,7 @@
 // but by using 0 as the day it will give us the last day of the prior
 // month. So passing in 1 as the month number will return the last day
 // of January, not February
+
 function daysInPrevMonth (month, year) {
     return new Date(year, month, 0).getDate();
 }
