e275d3a0061591e91e33c78b73c596f90ac27c2c
Rodrigo Tallar <rtallarr@gmail.com>
2024-02-22 03:57:55 -0300

    feat: add most played game with total hours played

--- js/api-calls/steam.js
+++ js/api-calls/steam.js
@@ -1,11 +1,31 @@
-const apiUrl = 'https://rtback.azurewebsites.net/steam/gameCount'
+const urlGameCount = 'https://rtback.azurewebsites.net/steam/gameCount'
+const urlMostPlayed = 'https://rtback.azurewebsites.net/steam/mostPlayed'
+
+const idGames = [
+    {"id": 252490, "name": "Rust"},
+    {"id": 1258080, "name": "Shop Titans"},
+    {"id": 311210, "name": "Call of Duty®: Black Ops III"}
+]
 
 async function getOwnedGames() {
-    const response = await fetch(apiUrl);
+    const response = await fetch(urlGameCount);
+    //console.log(response);
     return response.text();
 }
 
+async function getMostPlayed() {
+    const response = await fetch(urlMostPlayed);
+    const res = await response.json();
+    //console.log(res);
+    return res
+}
+
 getOwnedGames().then(data => 
-    //console.log(data)
+    //console.log(data),
     document.getElementById('gamesOwned').innerHTML = data
+)
+
+getMostPlayed().then(data => 
+    //console.log(idGames.find(x => x.id == data.appid)['name'] + ' ' + data.playtime_forever)
+    document.getElementById('mostPlayed').innerHTML = idGames.find(x => x.id == data.appid)['name'] + ' - ' + (data.playtime_forever / 60).toFixed(1) + ' hours'
 )
