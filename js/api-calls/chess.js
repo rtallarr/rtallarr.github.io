ed61c9ecbad5e49ef993b09b979ab3d49f4b3c19
rtallarr <rtallarr@gmail.com>
2024-08-12 15:33:33 -0400

    fix: function name for getting chess elo

--- js/api-calls/chess.js
+++ js/api-calls/chess.js
@@ -1,11 +1,11 @@
 const urlChessStats = 'https://api.chess.com/pub/player/rtallarr/stats'
 
-async function getMostPlayed() {
+async function getRapidElo() {
     const response = await fetch(urlChessStats);
     const res = await response.json();
     return res
 }
 
-getMostPlayed().then(data => 
+getRapidElo().then(data => 
     document.getElementById('chessRating').innerHTML = data.chess_rapid.last.rating
 )
