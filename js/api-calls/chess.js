f52ba546465338e9e69b25f02fe5e2580aa45d62
rtallarr <rtallarr@gmail.com>
2024-08-12 15:56:52 -0400

    feat: add rapid games played

--- js/api-calls/chess.js
+++ js/api-calls/chess.js
@@ -1,11 +1,12 @@
 const urlChessStats = 'https://api.chess.com/pub/player/rtallarr/stats'
 
-async function getRapidElo() {
+async function getChessData() { 
     const response = await fetch(urlChessStats);
-    const res = await response.json();
-    return res
+    return response.json()
 }
 
-getRapidElo().then(data => 
-    document.getElementById('chessRating').innerHTML = data.chess_rapid.last.rating
-)
+getChessData().then(data => {
+    var played = data.chess_rapid.record.draw+data.chess_rapid.record.win+data.chess_rapid.record.loss;
+    document.getElementById('chessRating').innerHTML = data.chess_rapid.last.rating;
+    document.getElementById('chessGames').innerHTML = played;
+})
