128ce72c1a426239b2d72afe6807c08d6d014011
unknown <rtallarr@gmail.com>
2025-03-31 16:13:17 -0300

    feat: add projects section
    add date to litlyx events

--- js/api-calls/github.js
+++ js/api-calls/github.js
@@ -1,11 +1,33 @@
-const apiLanguages = 'https://github-readme-stats.vercel.app/api/top-langs/?username=rtallarr&size_weight=0.5&count_weight=0.5'
-
-async function getLanguages() {
-    const response = await fetch(apiLanguages);
-    return response.text();
+async function getRepo(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
 
-//getLanguages().then(data => 
-//    //console.log(data)
-//    document.getElementById('svg-placeholder').innerHTML = data
-//)
+
+async function getProjects() {
+    const response = (await fetch('https://api.github.com/users/rtallarr/events/public')).json();
+    const pushEvents = (await response)
+        .filter(event => event.type === "PushEvent" || event.type === "PullRequestEvent")
+        .filter((event, index, self) => 
+            index === self.findIndex(e => e.repo.name === event.repo.name)
+        );
+
+    await Promise.all(pushEvents.map(async (event) => {
+        event.repo = await getRepo(event.repo.url);
+    }));
+
+    console.log("pushEvents:", pushEvents);
+    return pushEvents;
+}
+
+getProjects().then(data => 
+    data.forEach(event => {
+        $("#github").append(`
+            <div class="row">
+                <div class="col-5"><a href="${event.repo.html_url}" onclick="repoLinkEvent('${event.repo.full_name}')" target="_blank">${event.repo.full_name}</a></div>
+                <div class="col-7"><p>${event.repo.description || "No description provided"}</p></div>
+            </div>
+        `);
+    })
+);
--- js/utils/events.js
+++ js/utils/events.js
@@ -1,15 +1,39 @@
 function TopButtonEvent(link) {
+    let now = new Date();
+    let currentDate = now.toLocaleDateString('en-GB', { weekday: 'long', month: '2-digit', day: '2-digit' });
+    let currentTime = now.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    let date = currentDate + ' ' + currentTime;
+    console.log(date);
     Lit.event('Social Links', {
         metadata: {
-          'link': link
+          'link': link,
+          'date': date
         }
     });
 }
 
 function NavEvent(link) {
+    let now = new Date();
+    let currentDate = now.toLocaleDateString('en-GB', { weekday: 'long', month: '2-digit', day: '2-digit' });
+    let currentTime = now.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    let date = currentDate + ' ' + currentTime;
     Lit.event('Nav Links', {
         metadata: {
-          'link': link
+          'link': link,
+          'date': date
+        }
+    });
+}
+
+function repoLinkEvent(repo) {
+    let now = new Date();
+    let currentDate = now.toLocaleDateString('en-GB', { weekday: 'long', month: '2-digit', day: '2-digit' });
+    let currentTime = now.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    let date = currentDate + ' ' + currentTime;
+    Lit.event('Repo Links', {
+        metadata: {
+          'repo': repo,
+          'date': date
         }
     });
 }
